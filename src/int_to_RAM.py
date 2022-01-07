0ee173278c28e08ff51512566bc2b11f71ca257e
FissoreD <79260701+FissoreD@users.noreply.github.com>
2022-01-07 16:56:53 +0100

    #5 File to transform int to ram instructions

---
+++ src/int_to_RAM.py
@@ -0,0 +1,28 @@
+from typing import List
+from cantor_int import Int
+
+
+def int_to_RAM(n: int):
+    n = Int(n)
+    res: List[str] = []
+    cantor_couple = n.int_to_couple()
+    while cantor_couple != (0, 0):
+        current = Int(cantor_couple[0])
+        if current % 3 < 2:
+            res.append(
+                'R{} = R{} {} 1'.format(current.left(), current.right(), ['+', '-'][current % 3 == 1]))
+        else:
+            k = current.left()
+            op = current.right().left()
+            saut = current.right().right()
+            inst = ('F', 'B')
+            res.append(f'if R{k}!=0 THEN GOTO{inst[op%2==0]} {saut}')
+        cantor_couple = cantor_couple[1]
+    return '\n'.join(res)
+
+
+if __name__ == '__main__':
+    for i in range(1, 42):
+        print(i)
+        print(int_to_RAM(i))
+        print()
