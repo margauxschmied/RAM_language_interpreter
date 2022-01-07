00ab2baa84ea579d0498eb9830c39e9d6be01943
margauxschmied <margaux.schmied@icloud.com>
2022-01-07 15:53:10 +0100

    text editable #3

--- src/main.py
+++ src/main.py
@@ -1,8 +1,6 @@
 import tkinter as tk
 
 from tkinter.scrolledtext import ScrolledText
-from PySide6.QtWidgets import QMainWindow, QPlainTextEdit, QPushButton, QVBoxLayout, QWidget
-
 
 class Texte(tk.Text):
     def __init__(self, parent):
@@ -14,30 +12,6 @@ class Texte(tk.Text):
         self.grid(row=0, column=0, sticky="nsew")
         self.insert(tk.END, 'This is an example text.')
         self.pack(expand=True, fill='both')
-
-class MainWindow(QMainWindow):
-
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("RAM_language_interpreter")
-        self.btn = QPushButton("Execute")
-        self.btn.pressed.connect(self.start_process)
-        self.text = QPlainTextEdit()
-        # self.text.setReadOnly(True)
-
-        l = QVBoxLayout()
-        l.addWidget(self.btn)
-        l.addWidget(self.text)
-
-        w = QWidget()
-        w.setLayout(l)
-
-        self.setCentralWidget(w)
-
-    def start_process(self):
-        # We'll run our process here.
-        print(self.text.getPaintContext())
-        pass
 
 
 if __name__ == '__main__':
@@ -51,15 +25,3 @@ if __name__ == '__main__':
 
     root.mainloop()
 
-    # app = QApplication(sys.argv)
-    # # my_icon = QIcon()
-    # # my_icon.addFile('ressources/img/ramen.png')
-    # #
-    # # app.setWindowIcon(my_icon)
-    # app.setWindowIcon(QIcon("ressources/img/ramen.png"))
-    # app.setDesktopFileName("RAM_language_interpreter")
-    #
-    # w = MainWindow()
-    # w.show()
-    #
-    # sys.exit(app.exec())
