eb4c401d96c019714083cbe2a5a2daa948367a0b
FissoreD <79260701+FissoreD@users.noreply.github.com>
2022-01-08 14:01:45 +0100

    #8 Correction of decode instr add and sub

--- src/decode_int.py
+++ src/decode_int.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, cast
 from cantor_int import Int
 
 
@@ -11,18 +11,21 @@ def decode_int_instr(n: int):
         - IFB = IF Rk != 0 THEN GOTOB n (n = 3 <1, <k, <n, 0>>> + 2)
     """
     res: List[str] = []
-    current = Int(n)
-    if current % 3 < 2:
-        current = current / 3
+    n = Int(n)
+    if n % 3 < 2:
+        current = n / 3
         res.append(
-            'R{} = R{} {} 1'.format(current.left(), current.left(), ['+', '-'][current % 3 == 1]))
+            'R{} = R{} {} 1'.format(current, current, ['+', '-'][n % 3 == 1]))
     else:
-        current = current / 3
-        type_saut = current.left()
-        k = current.right().left()
-        saut = current.right().right()
-        inst = ('F', 'B')
-        res.append(f'if R{k}!=0 THEN GOTO{inst[type_saut==1]} {saut}')
+        try:
+            current = n / 3
+            k = current.left()
+            type_saut = current.right().left()
+            saut = current.right().right()
+            inst = ('F', 'B')
+            res.append(f'if R{k}!=0 THEN GOTO{inst[type_saut==1]} {saut}')
+        except Exception as e:
+            raise Exception(f'Cannot decode {n}')
     return '\n'.join(res)
 
 
@@ -37,14 +40,9 @@ def decode_int_program(n):
 
 
 if __name__ == '__main__':
-    L = [1002500825, 2, 5, 8, 11]
-    for elt in L:
-        print(f"Decoding instruction {elt}:")
+    for i in range(21):
         try:
-            print(decode_int_instr(elt))
-        except Exception as e:
-            print(e)
-    N = 1002500826
-
-    print(f"Decoding program {N} :")
-    print(decode_int_program(1002500826))
+            print(f"Decoding program {i} :")
+            print(decode_int_instr(i))
+        except:
+            print(f"Can't decode {i}")
