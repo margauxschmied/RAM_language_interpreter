5c8ff0e174bf561c881827f37a80c18326286c5a
Antoine Venturelli <ant.venturelli06@gmail.com>
2022-01-08 11:49:17 +0100

    (Last commit is for Save*) L'option New ouvre un nouveau fichier vierge intitré

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from tkinter import filedialog
+from tkinter import filedialog, ttk
 import tkinter as tk
 
 from tkinter.scrolledtext import ScrolledText
@@ -8,7 +8,6 @@ class Texte(ScrolledText):
     def __init__(self, parent):
         super().__init__(parent, width=20, height=10,
                          bg="lightgrey", wrap='word')
-        self.pack(expand=True, fill='both')
         self.add_text('This is an exemple text')
 
     def add_text(self, txt: str):
@@ -23,13 +22,14 @@ class Texte(ScrolledText):
 
 
 class main_class:
-    def __init__(self, root, text_editor) -> None:
+    def __init__(self, root) -> None:
         self.root = root
-        self.text_editor = text_editor
         self.initiate()
 
     def initiate(self):
         self.createMenu(self, root)
+        self.createNotebook(root)
+        self.text_editor.pack(expand=True, fill='both')
 
     def createMenu(self, x, parent):
         self.menu_bar = tk.Menu(parent)
@@ -78,7 +78,14 @@ class main_class:
         parent.config(menu=self.menu_bar)
 
     def new_file(self):
-        self.text_editor.clean()
+        self.acc += 1
+        self.titles.append('Untitled'+str(self.acc))
+        self.tab2 = tk.Frame()
+        self.frame2 = tk.Frame(self.notebook)
+        self.text_editor2 = Texte(self.frame2)
+        self.notebook.add(self.frame2, text=self.titles[1])
+        self.text_editor2.pack(expand=True, fill='both')
+        self.text_editor2.clean()
 
     def open_file(self):
         self.filename = filedialog.askopenfilename(
@@ -115,18 +122,31 @@ class main_class:
             f = open(self.filename, "w")
             f.write(program_text)
             f.close
+            self.notebook.tab(0, text=self.filename.split('/')[-1])
+
+    def createNotebook(self, parent):
+        self.acc = 1
+        self.notebook = ttk.Notebook(parent)
+        self.tab = tk.Frame()
+        self.frame = tk.Frame(self.notebook)
+        self.text_editor = Texte(self.frame)
+        self.titles = ["Untitled"+str(self.acc)]
+        self.notebook.add(self.frame, text=self.titles[0])
+
+        self.notebook.pack(fill=tk.BOTH, expand=1)
 
 
 def main(root):
-    text_editor = Texte(root)
-    main_class(root, text_editor)
+    #text_editor = Texte(root)
+    main_class(root)
+    #text_editor.pack(expand=True, fill='both')
 
 
 if __name__ == '__main__':
     root = tk.Tk()
     root.title("RAM_language_interpreter")
     root.iconphoto(False, tk.PhotoImage(file='img/ramen.png'))
-    # root.geometry("640x480")
+    root.geometry("640x480")
 
     main(root)
 
