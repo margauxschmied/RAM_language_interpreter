fe75f620b9a912e7eb816ea4b75a1c0331c0c887
Antoine Venturelli <ant.venturelli06@gmail.com>
2022-01-22 13:05:20 +0100

    little update

--- src/GUI/preprocessing.py
+++ src/GUI/preprocessing.py
@@ -43,7 +43,7 @@ def file_includes(text_editor, end_line, output):
 
             except PermissionError:
                 output.pretty_print(
-                    '[' + line + '] (line ' + MyGUI.idx_to_nb(index) + '): ', 'blue')
+                    '[' + line + '] (line ' + interface.MyGUI.idx_to_nb(index) + '): ', 'blue')
                 output.pretty_print(" Permission denied\n", 'red')
                 continue
 
@@ -67,11 +67,11 @@ def delete_preprocessing(text_editor):
         index = text_editor_temp.search(inc, '1.0', stopindex=tk.END)
         index2 = text_editor_temp.search(de, '1.0', stopindex=tk.END)
         if index != '':
-            index_end = MyGUI.idx_to_nb(index) + '.end'
+            index_end = interface.MyGUI.idx_to_nb(index) + '.end'
             text_editor_temp.delete(index, index_end)
 
         if index2 != '':
-            index_end = MyGUI.idx_to_nb(index2) + '.end'
+            index_end = interface.MyGUI.idx_to_nb(index2) + '.end'
             text_editor_temp.delete(index2, index_end)
     return text_editor_temp
 
@@ -85,13 +85,13 @@ def user_defines(text_editor, output):
     while index:
         index = text_editor_temp.search(de, '1.0', stopindex=tk.END)
         if index != '':
-            index_end = Frame.idx_to_nb(index) + '.end'
+            index_end = interface.MyGUI.idx_to_nb(index) + '.end'
             line = text_editor_temp.get(index, index_end)
             text_editor_temp.delete(index, index_end)
             words = line.split(' ')
             if len(words) != 3:
                 output.pretty_print(
-                    '[' + line + '] (line ' + Frame.idx_to_nb(index) + '): ', 'blue')
+                    '[' + line + '] (line ' + interface.MyGUI.idx_to_nb(index) + '): ', 'blue')
                 output.pretty_print(
                     " 2 parameters expected (found " + str(len(words) - 1) + ')\n', 'red')
                 continue
