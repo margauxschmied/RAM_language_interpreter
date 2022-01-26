42718657647bc37421868c273dcb63e07decc13e
margauxschmied <margaux.schmied@icloud.com>
2022-01-26 21:48:57 +0100

    fix color of text

--- src/GUI/widgets/texte.py
+++ src/GUI/widgets/texte.py
@@ -6,7 +6,7 @@ class texte(tk.Text):
 
     def __init__(self, parent):
         super().__init__(parent, width=20, height=10,
-                         bg="lightgrey", wrap='word')
+                         bg="lightgrey", wrap='word', fg='#000')
 
     def add_text(self, txt: str):
         self.insert(tk.END, txt)
